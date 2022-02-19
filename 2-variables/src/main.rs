ef721fbf0e4844fcecb0b5bf0098266848a6faf5
Brian Heise <bnheise@gmail.com>
2022-02-19 12:06:55 +0900

    setup code for chapter 2

--- 2-variables/src/main.rs
+++ 2-variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let my_name = "Tom";
+    let mut my_age = 39;
+    println!("Hello world!");
+    println!("My name is {}", my_name);
+    println!("I am {} years old", my_age);
+    my_age += 1;
+    println!("Next year I'll be {} years old", my_age);
 }
---
+++ 2-variables/src/selfIntroduction.js
@@ -0,0 +1,7 @@
+const myName = "Tom";
+let myAge = 39;
+console.log("Hello world!");
+console.log(`My name is ${myName}`);
+console.log(`I am ${myAge} years old`);
+myAge += 1;
+console.log(`Next year I'll be ${myAge} years old`);
